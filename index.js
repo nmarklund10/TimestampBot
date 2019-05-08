21dc274b7b970ae26ccf3fc48279e79a1da49ff7
Neil Marklund <nmarklund10@hotmail.com>
2019-05-07 20:03:11 -0400

    bluh

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.post('/webhook', (req, res) => {
       let sender_psid = webhook_event.sender.id;
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
+      console.log(webhook_event);
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
